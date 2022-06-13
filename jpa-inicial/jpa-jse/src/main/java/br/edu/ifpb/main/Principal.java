a8f417dca23b238579bda87261726b33f8928ee8
Ricardo Job <sousajob@gmail.com>
2022-06-13 20:35:01 -0300

    Projeto base para JPA

---
+++ jpa-inicial/jpa-jse/src/main/java/br/edu/ifpb/main/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.main;
+
+/**
+ * @author Ricardo Job
+ * @mail ricardo.job@ifpb.edu.br
+ * @since 06/06/2022, 19:55:37
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+    }
+}
